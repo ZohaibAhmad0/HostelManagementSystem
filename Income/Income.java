5245c4a206ee809a5cb83a1bd579a2c1c1179f51
Fahad <fahadashfaq996@gmail.com>
2024-05-04 23:28:20 +0500

    setter policies

--- Income/Income.java
+++ Income/Income.java
@@ -1,43 +1,50 @@
-public class Income
-{
-    private double totalExpectedIncome;
-    private double totalRentReceived;
-    private double totalrentPending;
+public class Income {
+    private Double totalExpectedIncome;
+    private Double totalRentReceived;
+    private Double totalRentPending;
 
-    public Income(double totalExpectedIncome, double totalRentReceived, double totalrentPending)
-    {
-       setTotalExpectedIncome(totalExpectedIncome);
-       setTotalrentPending(totalrentPending);
-       setTotalRentReceived(totalRentReceived);
+    public Income(Double totalExpectedIncome, Double totalRentReceived, Double totalRentPending) {
+        setTotalExpectedIncome(totalExpectedIncome);
+        setTotalRentReceived(totalRentReceived);
+        setTotalRentPending(totalRentPending);
     }
 
-    public double getTotalExpectedIncome()
-    {
+    public Double getTotalExpectedIncome() {
         return totalExpectedIncome;
     }
 
-    public void setTotalExpectedIncome(double totalExpectedIncome)
-    {
-        this.totalExpectedIncome = totalExpectedIncome;
+    public void setTotalExpectedIncome(Double totalExpectedIncome) {
+
+        if (totalExpectedIncome != null && totalExpectedIncome instanceof Number) {
+            this.totalExpectedIncome = totalExpectedIncome;
+        } else {
+            throw new IllegalArgumentException("Error: Total expected income must be a number");
+        }
     }
 
-    public double getTotalRentReceived()
-    {
+    public Double getTotalRentReceived() {
         return totalRentReceived;
     }
 
-    public void setTotalRentReceived(double totalRentReceived)
-    {
-        this.totalRentReceived = totalRentReceived;
+    public void setTotalRentReceived(Double totalRentReceived) {
+
+        if (totalRentReceived != null && totalRentReceived instanceof Number) {
+            this.totalRentReceived = totalRentReceived;
+        } else {
+            throw new IllegalArgumentException("Error: Total rent received must be a number");
+        }
     }
 
-    public double getTotalrentPending()
-    {
-        return totalrentPending;
+    public Double getTotalRentPending() {
+        return totalRentPending;
     }
 
-    public void setTotalrentPending(double totalrentPending)
-    {
-        this.totalrentPending = totalrentPending;
+    public void setTotalRentPending(Double totalRentPending) {
+
+        if (totalRentPending != null && totalRentPending instanceof Number) {
+            this.totalRentPending = totalRentPending;
+        } else {
+            throw new IllegalArgumentException("Error: Total rent pending must be a number");
+        }
     }
 }
